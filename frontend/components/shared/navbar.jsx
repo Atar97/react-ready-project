b88041bf93b44e161e002311e2e94d90f1c41c56
Austin <austin.cotant@gmail.com>
2018-11-07 10:10:36 -0800

    Update navbar

--- frontend/components/shared/navbar.jsx
+++ frontend/components/shared/navbar.jsx
@@ -5,9 +5,12 @@ class Navbar extends React.Component {
 
     render() {
         return <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="#">
-              Home
-            </a>
+            <Link class="navbar-brand" to="/">Home</Link>
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/card/find">Find Card</Link>
+                </li>
+            </ul>
           </nav>;
     }
 }
