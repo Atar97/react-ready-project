b649af3de3c5bb25e85c5815e8a7628dad9689fe
Austin <austin.cotant@gmail.com>
2018-11-07 10:03:43 -0800

    Fully integrated bootstrap

--- frontend/components/shared/navbar.jsx
+++ frontend/components/shared/navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class Navbar extends React.Component {
 
     render() {
-        return (
-            null
-        );
+        return <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="#">
+              Home
+            </a>
+          </nav>;
     }
 }
 
