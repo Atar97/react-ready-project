6efbe061a576b38349ca33a92943d20f72b683eb
Austin <austin.cotant@gmail.com>
2018-11-08 07:28:22 -0800

    Add Session and User actions

---
+++ frontend/actions/user_actions.js
@@ -0,0 +1,12 @@
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+import * as APIUtil from '../util/session_api_util';
+
+export const receiveUsers = users => ({
+    type: RECEIVE_USERS,
+    users
+})
+
+export const createUser = user => dispatch => {
+    return APIUtil.createUser(user)
+        .then(res => dispatch(receiveUsers(res)))
+};
