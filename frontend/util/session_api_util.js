ff185d36ddbbd7621483546ea9082ade79f330df
Austin <austin.cotant@gmail.com>
2018-11-08 07:34:30 -0800

    Add session api util so users can log in

--- frontend/util/session_api_util.js
+++ frontend/util/session_api_util.js
@@ -1,3 +1,12 @@
-export const login = (user) => {
-    
-}
+export const login = user => (
+  $.ajax({
+    url: "/session",
+    method: "POST",
+    data: user
+}));
+
+export const logout = () => (
+  $.ajax({
+    url: "/session",
+    method: "DELETE"
+  }));
